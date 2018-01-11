c52c1a3c0c10629ca5bc50e03323c85ee92db67c
Vlad Plotnikov <vladpdev@Vlads-MacBook-Pro.local>
2018-01-10 16:00:56 -0800

    call stack, callback, node API

--- playground/async-basics.js
+++ playground/async-basics.js
@@ -8,3 +8,17 @@ setTimeout(()=>{
     console.log('Another one')
 }, 0)
 console.log('Finishing up');
+
+let x = 1
+
+let y = x + 9
+
+console.log(`y is ${y}`)
+
+let add = (a, b) => {
+    let total = a + b;
+    return total;
+}
+
+let res = add(3,8)
+console.log(res)
---
+++ playground/notes.md
@@ -0,0 +1,1 @@
+#Call stack ==========> Node APIs =================> Callback Queue
